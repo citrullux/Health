8304c06cbdb266038b262b31ec5892d8c2d8a06c
Дмитрий Абдрахманов <565963@gmail.com>
2016-09-29 18:02:08 +0400

    Main window interface changes

--- Health Monitor/Health Monitor/MainWindow.Designer.cs
+++ Health Monitor/Health Monitor/MainWindow.Designer.cs
@@ -36,8 +36,12 @@
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnDel = new System.Windows.Forms.Button();
             this.btnChange = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
-            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // grid
@@ -52,17 +56,11 @@
             this.grid.ReadOnly = true;
             this.grid.RowHeadersVisible = false;
             this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.grid.Size = new System.Drawing.Size(984, 425);
+            this.grid.Size = new System.Drawing.Size(824, 425);
             this.grid.TabIndex = 0;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.btnTable);
-            this.groupBox1.Controls.Add(this.btnExp);
-            this.groupBox1.Controls.Add(this.btnImp);
-            this.groupBox1.Controls.Add(this.btnAdd);
-            this.groupBox1.Controls.Add(this.btnDel);
-            this.groupBox1.Controls.Add(this.btnChange);
             this.groupBox1.Location = new System.Drawing.Point(692, 443);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(304, 114);
@@ -71,7 +69,7 @@
             // 
             // btnTable
             // 
-            this.btnTable.Location = new System.Drawing.Point(137, 77);
+            this.btnTable.Location = new System.Drawing.Point(843, 218);
             this.btnTable.Name = "btnTable";
             this.btnTable.Size = new System.Drawing.Size(153, 23);
             this.btnTable.TabIndex = 5;
@@ -81,9 +79,9 @@
             // 
             // btnExp
             // 
-            this.btnExp.Location = new System.Drawing.Point(137, 48);
+            this.btnExp.Location = new System.Drawing.Point(6, 48);
             this.btnExp.Name = "btnExp";
-            this.btnExp.Size = new System.Drawing.Size(153, 23);
+            this.btnExp.Size = new System.Drawing.Size(134, 23);
             this.btnExp.TabIndex = 4;
             this.btnExp.Text = "Экспорт данных";
             this.btnExp.UseVisualStyleBackColor = true;
@@ -91,9 +89,9 @@
             // 
             // btnImp
             // 
-            this.btnImp.Location = new System.Drawing.Point(137, 19);
+            this.btnImp.Location = new System.Drawing.Point(6, 19);
             this.btnImp.Name = "btnImp";
-            this.btnImp.Size = new System.Drawing.Size(153, 23);
+            this.btnImp.Size = new System.Drawing.Size(134, 23);
             this.btnImp.TabIndex = 3;
             this.btnImp.Text = "Импорт данных";
             this.btnImp.UseVisualStyleBackColor = true;
@@ -103,7 +101,7 @@
             // 
             this.btnAdd.Location = new System.Drawing.Point(6, 19);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(122, 23);
+            this.btnAdd.Size = new System.Drawing.Size(134, 23);
             this.btnAdd.TabIndex = 2;
             this.btnAdd.Text = "Добавить запись";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -113,7 +111,7 @@
             // 
             this.btnDel.Location = new System.Drawing.Point(6, 77);
             this.btnDel.Name = "btnDel";
-            this.btnDel.Size = new System.Drawing.Size(122, 23);
+            this.btnDel.Size = new System.Drawing.Size(134, 23);
             this.btnDel.TabIndex = 1;
             this.btnDel.Text = "Удалить запись";
             this.btnDel.UseVisualStyleBackColor = true;
@@ -123,26 +121,67 @@
             // 
             this.btnChange.Location = new System.Drawing.Point(6, 48);
             this.btnChange.Name = "btnChange";
-            this.btnChange.Size = new System.Drawing.Size(122, 23);
+            this.btnChange.Size = new System.Drawing.Size(134, 23);
             this.btnChange.TabIndex = 0;
             this.btnChange.Text = "Изменить запись";
             this.btnChange.UseVisualStyleBackColor = true;
             this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnAdd);
+            this.groupBox2.Controls.Add(this.btnChange);
+            this.groupBox2.Controls.Add(this.btnDel);
+            this.groupBox2.Location = new System.Drawing.Point(843, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(153, 109);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Управление записями";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnExp);
+            this.groupBox3.Controls.Add(this.btnImp);
+            this.groupBox3.Location = new System.Drawing.Point(843, 127);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(152, 85);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Импорт/Экспорт";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(849, 247);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 183);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Внимание! Созданную диагностическую таблицу ни в коем случае нельзя импортировать" +
+    " с помощью функции импорта.";
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 562);
+            this.ClientSize = new System.Drawing.Size(1008, 442);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.btnTable);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.grid);
-            this.MaximumSize = new System.Drawing.Size(1024, 600);
-            this.MinimumSize = new System.Drawing.Size(1024, 600);
+            this.MaximumSize = new System.Drawing.Size(1024, 480);
+            this.MinimumSize = new System.Drawing.Size(1024, 480);
             this.Name = "MainWindow";
             this.Text = "Определение физкультурной группы (средний школьный возраст 11-14 лет)";
             ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
-            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -156,5 +195,8 @@
         private System.Windows.Forms.Button btnExp;
         private System.Windows.Forms.Button btnImp;
         private System.Windows.Forms.Button btnTable;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
